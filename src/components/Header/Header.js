baf36b42ecd165d66125620900c8de244c498da8
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-10-28 01:52:59 -0300

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,22 @@ import * as S from './styles';
 const Header = ({ score }) => {
   return (
     <S.Header id="header">
-      <h1>Where's...</h1>
-      <p>How to play</p>
-      <p>GitHub</p>
+      <h1>
+        <a href="/">Hidden5</a>
+      </h1>
+      <S.NavContainer>
+        <S.NavItem>How to play</S.NavItem>
+        <S.NavItem>Leaderboards</S.NavItem>
+        <S.NavItem>
+          <a
+            href="https://github.com/llfalcao/photo-tagging"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </S.NavItem>
+      </S.NavContainer>
       <Scoreboard current={score.current} max={score.max} />
     </S.Header>
   );
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -4,4 +4,14 @@ export const Header = styled.header`
   background: #101112;
   color: #fff;
   display: flex;
+  padding: 1rem 0;
+  filter: drop-shadow(0 0 4px #000);
+`;
+
+export const NavContainer = styled.nav``;
+
+export const NavItem = styled.li`
+  display: inline-block;
+  margin: 0 1.5rem;
+  cursor: pointer;
 `;
